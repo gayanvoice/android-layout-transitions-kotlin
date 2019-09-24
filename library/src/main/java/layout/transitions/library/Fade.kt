a0d98eb2423a12e045f3e86c2223723affaefa10
gayankuruppu <example@example.com>
2019-09-24 17:13:40 +0530

    Update App

--- library/src/main/java/layout/transitions/library/Fade.kt
+++ library/src/main/java/layout/transitions/library/Fade.kt
@@ -4,4 +4,9 @@ class Fade {
     fun In(): Array<Int>{
         return arrayOf(R.anim.fade_in, R.anim.fade_out)
     }
+
+    fun Out(): Array<Int>{
+        return arrayOf(R.anim.fade_in, R.anim.fade_out)
+    }
+
 }
